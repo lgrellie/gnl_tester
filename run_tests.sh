3384a199a1e2f532bfb0551d19583224125615d8
Leonard GRELLIER <lgrellie@student.42.fr>
2019-10-22 18:34:29 +0200

    Ignore user project files. run_tests script now generates tests and cleans them, and compiles with user files

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-
-./a.out tests/*.txt;
-for file in tests/*.txt; do
-	diff -q "$file" "output/output_$(basename $file)" >> diffs;
-done
-[ -s diffs ] && cat diffs || echo "No diffs found between input and output. GG <3 !!";
+if [[ "$#" -ne 1 ]]; then
+	echo "./run_tests \$BUFFER_SIZE";
+else
+	gcc main.c get_next_line.c get_next_line_utils.c -D BUFFER_SIZE=$1;
+	./generate_tests.sh;
+	./a.out tests/*.txt;
+	for file in tests/*.txt; do
+		diff -q "$file" "output/output_$(basename $file)" >> diffs;
+	done
+	[ -s diffs ] && cat diffs || echo "No diffs found between input and output. GG <3 !!";
+	./clean.sh;
+fi
